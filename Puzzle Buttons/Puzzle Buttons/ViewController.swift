1c0c734340b1514d755f02b8b4814838b2178d1b
Ryley Herrington <ryleyherrington@gmail.com>
2016-04-20 21:56:34 -0700

    Eh, working and looks different than it did.

--- Puzzle Buttons/Puzzle Buttons/ViewController.swift
+++ Puzzle Buttons/Puzzle Buttons/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var n:Int = 4 //number of rows / columns
+    var n:Int = 3 //number of rows / columns
     var width:CGFloat = 0
     var height:CGFloat = 0
     var nonhighlighted = UIColor ( red: 0.4789, green: 0.0, blue: 0.4788, alpha: 1.0 )
@@ -18,27 +18,57 @@ class ViewController: UIViewController {
     var onArr:[Bool] = []
     var buttonArray:[UIButton] = []
     
+    //IBOutlets
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var numLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.width = self.view.frame.size.width
-        self.height = self.view.frame.size.height
+        self.width  = self.view.frame.size.width 
+        self.height = self.containerView.frame.size.height
+        
         setupButtons()
+        setupSlider()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    func createAndAddButton(index:Int, row:Int) ->UIButton{
+    func setupSlider(){
+        self.slider.minimumValue = 3.0
+        self.slider.maximumValue = 5.0
+        self.slider.minimumTrackTintColor = UIColor ( red: 0.0, green: 0.6961, blue: 0.6382, alpha: 1.0 )
+        self.slider.maximumTrackTintColor = UIColor ( red: 0.0, green: 0.6961, blue: 0.6382, alpha: 1.0 )
+        self.slider.setValue(3.0, animated: true)
+    }
+    
+    @IBAction func sliderChanged(sender: UISlider) {
+        self.numLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func startGame(sender: AnyObject) {
+        self.n = Int(self.slider.value)
+        removeAllButtons()
+        setupButtons()
+    }
+    
+    func removeAllButtons() {
+        for b in self.buttonArray {
+            b.removeFromSuperview()
+        }
+        
+        self.buttonArray.removeAll()
+        self.onArr.removeAll()
+    }
+    
+    func createAndAddButton(index:Int, row:Int, w:CGFloat, h:CGFloat) ->UIButton{
         let button = UIButton(type: .System)
         
-        
-        let w = (width-(10.0*(CGFloat(n)+1.0)))/CGFloat(n)
-        let h = CGFloat(80)
-        let x = 10 + (10.0*(CGFloat(index%n)))+CGFloat((CGFloat(index % n)+1.0)*w) - w
-        let y = CGFloat((row+1)*90)
-     print("index:\(index) \(row)\t \(x)\t \(y)")
+        let x = (10.0*(CGFloat(index%n)))+CGFloat((CGFloat(index % n)+1.0)*w) - w
+        let y = CGFloat((row)*90)
         
         button.frame = CGRectMake(x, y, w, h)
         button.backgroundColor = self.nonhighlighted
@@ -46,20 +76,22 @@ class ViewController: UIViewController {
         button.tag = index
         button.addTarget(self, action: #selector(ViewController.buttonTouched(_:)), forControlEvents: UIControlEvents.TouchUpInside)
         
-        self.view.addSubview(button)
+        self.containerView.addSubview(button)
         return button
     }
 
     func setupButtons() {
+        let w = (width-(10.0*(CGFloat(n)+1.0)))/CGFloat(n)
+        let h = CGFloat(80.0)
+        
         for i in 0..<n*n {
 //            print("i:\(i) n=\(n), i%n = \(i%n), row=\(trunc(Double(i/n)))")
-            self.buttonArray.append(createAndAddButton(i, row: Int(trunc(Double(i/n)))))
+            self.buttonArray.append(createAndAddButton(i, row: Int(trunc(Double(i/n))), w:w, h:h))
             onArr.append(false)
         }
     }
    
     func toggleButton(index:Int) {
-        print("toggle \(index)")
         let button = buttonArray[index]
         if button.backgroundColor == self.nonhighlighted {
             button.backgroundColor = self.highlighted
